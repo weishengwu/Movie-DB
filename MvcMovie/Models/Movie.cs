100b5158891ea9b08bbeac53699886047b2dd48b
Weisheng Wu <weishenggwuu@gmail.com>
2019-04-21 02:20:34 -0700

    filled in rating NULL values

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -11,6 +11,34 @@ namespace MvcMovie.Models
     {
         public int Id { get; set; }
 
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        public string Title { get; set; }
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+        [Required]
+        [StringLength(30)]
+        public string Genre { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [StringLength(5)]
+        [Required]
+        public string Rating { get; set; }
+    }
+    /*
+    public class Movie
+    {
+        public int Id { get; set; }
+
         //[Display(Name = "Hello")]
         public string Title { get; set; }
 
@@ -24,4 +52,5 @@ namespace MvcMovie.Models
         public decimal Price { get; set; }
         public string Rating { get; set; }
     }
+    */
 }
